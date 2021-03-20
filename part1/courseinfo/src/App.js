dd70c321c5a03349bac7822fb50a3acb03503580
Kayra <kayrabtuncer@gmail.com>
2021-03-20 19:02:05 +0300

    step 1
    The project was created and the first step was taken.

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.props1 + props.props2 + props.props3}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total props1={exercises1} props2={exercises2} props3={exercises3} />
+    </>
+  )
+}
+
+export default App
