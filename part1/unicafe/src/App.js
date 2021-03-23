df24a116634b5cf9a61841cd1943e0669174bf70
Kayra <kayrabtuncer@gmail.com>
2021-03-23 17:51:12 +0300

    exercises 1.6
    Created unicafe feedback page

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <h1>statistics</h1>
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad}
+      </p>
+    </>
+  )
+}
+
+export default App
