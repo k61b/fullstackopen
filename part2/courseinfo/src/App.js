2284caedd1c23db8f296671ed4ae0633f92081ef
Kayra <kayrabtuncer@gmail.com>
2021-03-25 15:23:10 +0300

    exercises 2.2
    Total component added.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -24,11 +24,19 @@ const Content = ({ course }) => {
   )
 }
 
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </>
   )
 }
