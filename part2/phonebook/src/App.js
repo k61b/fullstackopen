87669a4d8ee4da5f01f98baa43344d08b3dcba91
Kayra <kayrabtuncer@gmail.com>
2021-03-26 14:26:18 +0300

    exercises 2.6
    Adding new person has been applied.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{ listStyle: 'none' }}>
+        {persons.map((e, i) => <li key={i}>{e.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+ReactDOM.render(<App />,
+  document.getElementById('root'))
