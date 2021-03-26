076ce9295cc27bd904861e859659f6c420551b7f
Kayra <kayrabtuncer@gmail.com>
2021-03-26 16:38:42 +0300

    exercises 2.7 & 2.8
    Includes control has been done and number has been added.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,22 +2,34 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '+905554445522'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
+    const personsArray = persons.map(e => e.name)
     const personObject = {
       name: newName,
+      number: newNumber
     }
-
-    setPersons(persons.concat(personObject))
+    personsArray.includes(`${personObject.name}`) ?
+      alert(`${newName} is already added to phonebook`) :
+      setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -28,6 +40,10 @@ const App = () => {
           name: <input
             value={newName}
             onChange={handleNameChange}
+          /><br />
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
@@ -35,9 +51,16 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul style={{ listStyle: 'none' }}>
-        {persons.map((e, i) => <li key={i}>{e.name}</li>)}
-      </ul>
+      <table>
+        <tbody>
+          {persons.map((e, i) =>
+            <tr key={i}>
+              <td>{e.name}</td>
+              <td>{e.number}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
