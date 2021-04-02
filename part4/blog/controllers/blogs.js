a27a5eb4711b381871b8f96cfc5de03724229b8f
Kayra <kayrabtuncer@gmail.com>
2021-04-02 13:53:05 +0300

    exercises 4.1-4.2
    setup complete

---
+++ part4/blog/controllers/blogs.js
@@ -0,0 +1,21 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
---
+++ part4/blog/utils/logger.js
@@ -0,0 +1,11 @@
+const info = (...params) => {
+  console.log(...params)
+}
+
+const error = (...params) => {
+  console.log(...params)
+}
+
+module.exports = {
+  info,error
+}
