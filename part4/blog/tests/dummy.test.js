cd4dde8d66ad2ca9a85f35c841aac1b905c9f211
Kayra <kayrabtuncer@gmail.com>
2021-04-02 17:03:40 +0300

    exercises 4.3 & 4.4
    helper functions and unit tests

---
+++ part4/blog/tests/dummy.test.js
@@ -0,0 +1,9 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
