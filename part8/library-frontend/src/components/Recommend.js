5c331872dd650916137a990bbd29d6609e925fc1
kayraberktuncer <kayrabtuncer@gmail.com>
2021-06-03 21:09:33 +0300

    exercises 8.21&8.22

--- part8/library-frontend/src/components/Recommend.js
+++ part8/library-frontend/src/components/Recommend.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import { useQuery } from '@apollo/client'
-import { ME } from '../queries'
+import { useQuery, useLazyQuery } from '@apollo/client'
+import { ME, ALL_BOOKS } from '../queries'
 import BooksTable from './BooksTable'
 
 const Recommend = (props) => {
-  const books = props.books
+  const meResult = useQuery(ME)
+  const [getBooks, result] = useLazyQuery(ALL_BOOKS, {
+    fetchPolicy: 'no-cache',
+  })
   const [me, setMe] = useState(null)
   const [meBooks, setMeBooks] = useState([])
 
-  useQuery(ME, {
-    onCompleted: ({ me }) => {
-      setMe(me)
-    },
-  })
+  useEffect(() => {
+    if (meResult.data) {
+      setMe(meResult.data.me)
+      getBooks({ variables: { genre: meResult.data.me.favoriteGenre } })
+    }
+  }, [meResult, me, getBooks])
 
   useEffect(() => {
-    setMeBooks(books.filter((book) => book.genres.includes(me.favoriteGenre)))
-  }, [me, books])
+    if (result.data) setMeBooks(result.data.allBooks)
+  }, [result])
 
   if (!props.show) {
     return null
@@ -25,7 +29,10 @@ const Recommend = (props) => {
   return (
     <div>
       <h2>recommendations</h2>
-      <p>books in your favorite genre patterns</p>
+      <p>
+        books in your favorite genre patterns:{' '}
+        <strong>{me.favoriteGenre}</strong>
+      </p>
       <BooksTable books={meBooks} />
     </div>
   )
